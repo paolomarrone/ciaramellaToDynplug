76a939ce99641c556c3a02b7991f42f70c0ffdb7
Paolo Marrone <99246960+paolomarrone@users.noreply.github.com>
2023-11-24 12:19:01 +0100

    beautify test.js

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 #!/bin/node
 
 // Test
-console.log("Testing...")
+console.log("Testing...");
 
-const compiler = require('zampogna')
-const crmToDynplug = require('./crmToDynplug')
+const compiler = require('zampogna');
+const crmToDynplug = require('./crmToDynplug');
 
-const code = 'yl, yr = A(xl, xr, vl, vr) { yl = xl * vl \nyr = xr * vr \n} '
+const code = `
+	yl, yr = A(xl, xr, vl, vr) { 
+ 		yl = xl * vl 
+   		yr = xr * vr 
+	}
+`;
 
 crmToDynplug.ciaramellaToDynplug(compiler, code, 'A', ['vl', 'vr'], '', "localhost", 10002, "x86_64", "localhost", 10001, 
 	function (msg) {
